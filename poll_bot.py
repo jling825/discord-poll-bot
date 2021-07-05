30af227bc1e5731238c35fa2c82a423688fa900b
jeremyling <jling825@gmail.com>
2021-07-05 15:25:49 -0700

    Setup bot to be always online

--- poll_bot.py
+++ poll_bot.py
@@ -1,14 +1,16 @@
-import os
 import config
+import send_ping
+
+import os
 import discord
 
 # get Discord token
 try:
-    bot_token = config.bot_token # from local config.py
-    print("Getting token from congif.py")
+    bot_token = config.bot_token  # from local config.py
+    execution_location = 'locally'
 except:
-    bot_token = os.environ['bot_token'] # from repl.it secret
-    print("Getting token from repl.it secret")
+    bot_token = os.environ['bot_token']  # from repl.it secret
+    execution_location = 'online'
 
 client = discord.Client()
 
@@ -19,21 +21,26 @@ async def embed(ctx, poll_title):
     embed=discord.Embed(title=poll_title)
 '''
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith('!execution'):
+        await message.channel.send('Currently executed ' + execution_location)
+
     if message.content.startswith('!startpoll'):
-        split_msg = message.content.split(' ',1)
+        split_msg = message.content.split(' ', 1)
         if len(split_msg) == 1:
             await message.channel.send('Accepting voting options for ' + message.author.name + '\'s poll')
         else:
             await message.channel.send('Accepting voting options for ' + split_msg[1])
-        
 
+send_ping.ping()  # disable this to test locally
 client.run(bot_token)
