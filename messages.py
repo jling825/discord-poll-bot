5ed7d56b286080b5db0bf3868f6d1f2f3d3b6def
jeremyling <jling825@gmail.com>
2021-08-08 12:40:18 -0700

    Moved !status command after !blacklist

--- messages.py
+++ messages.py
@@ -1,14 +1,14 @@
 # general responses
-commands = ["!status", "!whitelist", "!blacklist", "!newpoll", "!polltitle", "!polldesc", "!pollurl", "!polladd", "!polldrop", "!tally"]
+commands = ["!whitelist", "!blacklist", "!status", "!newpoll", "!polltitle", "!polldesc", "!pollurl", "!polladd", "!polldrop", "!tally"]
 
 help = (
-"`!status` - Prints bot's local/online statuses\n" +
 "`!whitelist` - Add channel to Poll Bot whitelist\n" +
 "`!blacklist` - Remove channel from Poll Bot whitelist\n" +
+"`!status` - Prints bot's local/online statuses\n" +
 "`!newpoll` - Starts a new poll\n" +
-"`!polltitle` - Assigns poll title\n" +
-"`!polldesc` - Assigns poll description\n" +
-"`!pollurl` - Assigns poll url\n" +
+"`!polltitle` - Updates poll title\n" +
+"`!polldesc` - Updates poll description\n" +
+"`!pollurl` - Updates poll url\n" +
 "`!polladd` - Adds voting option to poll\n" +
 "`!polldrop` - Removes voting option from poll\n" +
 "`!tally` - Start tally"
